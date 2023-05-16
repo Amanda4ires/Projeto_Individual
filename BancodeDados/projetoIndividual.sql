1810ba78292339acd8835550b6f7cd4b7ee5cc50
amanda4ires <amanda.amaro@sptech.school>
2023-05-15 23:58:36 -0300

    adicionei tela de cadastro/atualizei banco de dados

--- BancodeDados/projetoIndividual.sql
+++ BancodeDados/projetoIndividual.sql
@@ -1,27 +1,35 @@
 create database projetoIndividual;
+
 use projetoIndividual;
 create table usuario(
 idUsuario int primary key auto_increment,
 nomeCompleto varchar (45),
+dtNasc date, 
 celular char (11),
-email varchar(45),
-senha varchar(30)
+emailPessoal varchar(45)
 ); 
 insert into usuario values 
-(null,'Ana Beatriz Aires Pereira', '11940987546'),
-(null,'Giovanna Cirineu Amaro' ,'11934265187'),
-(null,'Alice Regina Zacarias','11965342987');
+(null,'Ana Beatriz Aires Pereira', '11940987546','ana.pereira@gmail.com'),
+(null,'Giovanna Cirineu Amaro' ,'11934265187','giovanna.amaro@gmail.com'),
+(null,'Alice Regina Zacarias','11965342987','alice.zacarias@gmail.com');
 
-create table contaUsuario(
+create table cadastro(
 idConta int primary key auto_increment,
 apelido varchar(45),
+pesoAtual float,
 email varchar(45),
-senha varchar(30));
+senha varchar(30),
+fksuario int,
+constraint fkdependente foreign key (fkUsuario) references
+usuario(idUsuario),
+constraint fkExDieta foreign key (fkDieta) references
+exemploDieta(idDieta)
+);
 
-insert into contaUsuario value
-(null,'Aninha','aninha@gmail.com','ajsjhgdg'),
-(null,'Gigi','Gih@outlook.com','jfknjvbh'),
-(null,'Regina','alice.R@hotmail.com','sjnbdhhv');
+insert into cadastro value
+(null,'Aninha','78,4','aninha@gmail.com','ajsjhgdg'),
+(null,'Gigi','60,5','Gih@outlook.com','jfknjvbh'),
+(null,'Regina','65,2','alice.R@hotmail.com','sjnbdhhv');
 
 create table exemploDieta(
 idDieta int primary key auto_increment,
@@ -32,7 +40,7 @@ descrição varchar(500)
 
 insert exemploDieta value
 (null,'Emagrecimento','Dicas de treinos e exemplos de dietas para emagrecimento '),
-(null,'Ganho de massa','Dicas de treinos e exemplos de dietas para ganho de massa magra'),
+(null,'manutenção de peso','Dicas de treinos e exemplos de dietas para ganho de massa magra'),
 (null,'Proteica','Dicas de treinos e exemplos de dietas para hipertrofia');
 
 
